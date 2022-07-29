c39f1fe250f3e75546928f5ad44fd5c1f1e88654
JoeBlackNinja <jose.nava@alumnos.udg.mx>
2022-07-29 14:38:03 -0500

    first commit 327

---
+++ pages/api/categories.js
@@ -0,0 +1,14 @@
+//Utiliza la consulta en la API al manejar estados Redu<, useState, 
+//useContext -- BUENAS PRACTICAS
+
+import { PrismaClient } from '@prisma/client';
+
+export default async function handler(req, res) {
+  
+  const prisma = new PrismaClient();
+
+  const categories = await prisma.category.findMany();
+
+  res.status(200).json(categories)
+}
+
